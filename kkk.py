bab3404df917e39525df4bbdc01f59aa6de9dbe8
Gohvvftt66 <loncacdume888@gmail.com>
2025-06-09 06:41:02 +0700

    Update kkk.py

--- kkk.py
+++ kkk.py
@@ -840,7 +840,7 @@ def Main():
                                     dem+=1
                                     time = datetime.now().strftime("%H:%M:%S")
                                     print(f"\033[1;31m|\033[1;36mPhuocDEV\033[1;31m|\033[1;32mLIKEVIP\033[1;31m|\033[1;35m+1100xu\033[1;31m|\033[1;35m{uid_hidden}\033[1;31m|\033[1;33m{xu}\033[1;31m|\033[1;34m{user}\033[1;31m|\033[1;36m{time}")
-				         loilike = 0
+				        loilike = 0
                                     if dem % doinick == 0:
                                         ptool = 1
                                         break
@@ -914,7 +914,7 @@ def Main():
                                     xu = get.split('"soduchinh">')[1].split('<')[0]
                                     dem+=1
                                     time = datetime.now().strftime("%H:%M:%S")
-                                    print(f'[{dem:04d}][{time}][{uid_hidden}][+300][{xu}][LIKETHUONG]')
+                                    print(f'\033[1;31m|{dem:04d}\033[1;31m|\033[1;36mPhuocDEV\033[1;31m|\033[1;32mLIKETHUONG\033[1;31m|\033[1;35m+600xu\033[1;31m|\033[1;35m{uid_hidden}\033[1;31m|\033[1;33m{xu}\033[1;31m|\033[1;34m{user}\033[1;31m|\033[1;36m{h}:{m}:{s}")
                                         loilike = 0
                                     if dem % doinick == 0:
                                         ptool = 1
